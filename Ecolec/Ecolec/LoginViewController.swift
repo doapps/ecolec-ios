ff8094cb7668e4b39a02bd93e5ded52295e49ab7
Nicolas <nicolas@Mac-mini-de-Nicolas.local>
2019-06-01 11:23:43 -0500

    Diseñar registro y login

--- Ecolec/Ecolec/LoginViewController.swift
+++ Ecolec/Ecolec/LoginViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func openHome(_ sender: UITapGestureRecognizer) {
+        let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        present(homeVC!, animated: true)
     }
-    */
-
 }
